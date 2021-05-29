2d9983711ae3962acbdb1c069c638de754c1bbfa
Mohammad Daimi <mhdaimi@hotmail.com>
2021-05-29 15:43:46 +0530

    incorporate review comment

--- src/code/HelloWorld.java
+++ src/code/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Changes for some requirement");
-		
+		System.out.println("New Statement");
 	}
 
 }
